eb8d91bde0cc97956b5b956d46a7824720901091
khuong2924 <truongkhuong666@gmail.com>
2025-06-15 03:16:53 +0700

    update cors

--- g-scores-be/config/initializers/cors.rb
+++ g-scores-be/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost', 'http://localhost:8080'
+    origins ENV.fetch('ALLOWED_ORIGINS', '').split(',')
 
     resource '*',
       headers: :any,
